abbdfdc68cb4b017d4359990e6c98a56daaf32d4
Luis Arrieta <luisarrieta796@gmail.com>
2024-10-11 10:21:02 -0500

    Install and configure TailwindCSS, Shadcn and Button shadcn component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Button } from '@/components/ui/button'
+import { RocketIcon } from 'lucide-react'
+
 export default function App () {
   return (
-    <div>App</div>
+    <div className='w-full h-screen bg-indigo-900 grid place-content-center gap-5'>
+      <h1 className='text-indigo-300 text-5xl font-semibold'>Hi TailwindCSS</h1>
+      <Button size='lg' variant='secondary'>
+        <RocketIcon className='mr-2' size={20} /> Shadcn Button
+      </Button>
+    </div>
   )
 }
