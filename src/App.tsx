b8809b87cfefd9e5d985bba970164a3ec35aa4ad
Luis Arrieta <luisarrieta796@gmail.com>
2024-10-13 23:37:41 -0500

    Install and configure nProgress loader

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,18 @@ import PasswordRecovery from '@/components/Auth/PasswordRecovery'
 import Home from '@/components/Pages/Home'
 import Account from '@/components/Profile/Account'
 import { Route, Switch } from 'wouter'
+import nProgress from 'nprogress'
+import { useEffect } from 'react'
 
 export default function App () {
+  nProgress.start()
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      nProgress.done()
+    },1300)
+  },[])
+  
   return (
     <Switch>                                                                                                                             <Route path='/' component={Home} />
       <Route path='/' component={Account} />
